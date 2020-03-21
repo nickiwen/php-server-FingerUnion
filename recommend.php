9f2f2dc3b3e1432a22e129e21c1983d0dd3278ee
nickiwen <iwen@iwenwiki.com>
2020-03-21 23:50:20 +0800

    php服务器资源

---
+++ recommend.php
@@ -0,0 +1,12 @@
+<?php
+    $url = "https://m.qyer.com/home/api/biu_list?ajaxID=5cd8d69ef1d508c32233475c&page=1";
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $file_contents = curl_exec($ch);
+    curl_close($ch);
+    echo $file_contents;
